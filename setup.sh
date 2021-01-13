37dd9f2161b3a2fe3f51e9dc3f20bce57b1d98b7
sharifmehedi24 <sharifmehedi24@yahoo.com>
2021-01-13 12:00:46 +0600

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 apt install util-linux
+mv ./.cal-extnd ~/
+cd ~/.cal-extnd
+touch calfile
 cd ~
-mkdir .cal_extnd
-cd .cal-extnd
-touch CAL_FILE
+if [ -f .bash_profile ]
+then
+	echo "alias cal=\"bash ~/.cal-extnd/cal-extnd.sh\"" >> .bash_profile
+else
+	touch .bash_profile
+	echo "alias cal=\"bash ~/.cal-extnd/cal-extnd.sh\"" >> .bash_profile
+fi
+source .bash_profile
+
