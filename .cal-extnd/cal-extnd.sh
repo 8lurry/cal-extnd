cbe1132018cfc3b3ae6888a17f22e7d6529915f9
sharifmehedi24 <sharifmehedi24@yahoo.com>
2021-01-14 11:23:43 +0600

    update

--- .cal-extnd/cal-extnd.sh
+++ .cal-extnd/cal-extnd.sh
@@ -8,42 +8,79 @@ ARGS[1]="-B"
 ARGS[2]="-l"
 ARGS[3]="-w"
 ARGS[4]="-E"
-
+touch ~/.cal-extnd/.buflike
+XEND=0
+MONTH[0]="January"
+MONTH[1]="February"
+MONTH[2]="March"
+MONTH[3]="April"
+MONTH[4]="May"
+MONTH[5]="June"
+MONTH[6]="July"
+MONTH[7]="August"
+MONTH[8]="September"
+MONTH[9]="October"
+MONTH[10]="November"
+MONTH[11]="December"
 if [ $# -gt 0 ]
 then
-	n=1
+	r=0
+	i=0
+	re="^[0-9]+$"
 	for arg in $*
 	do
-		if [ $arg -eq ${ARGS[0]} ]
+		if ! [ $arg -eq ${ARGS[0]} -o $arg -eq ${ARGS[1]} -o $arg -eq ${ARGS[2]} -o $arg -eq ${ARGS[3]} -o $arg -eq ${ARGS[4]} -o $arg =~ $re]
 		then
-			#dosomethimg
-		elif [ $arg -eq ${ARGS[1]} ]
-		then
-			#dosomething
-		elif [ $arg -eq ${ARGS[2]} ]
-		then
-			#dosomething
-		elif [ $arg -eq ${ARGS[3]} ]
-		then
-			#dosomething
-		elif [ $arg -eq ${ARGS[4]} ]
-		then
-			#dosomething
-			bash mkapntmnt.sh
-		else
-			r=0
-			RIM[$r]=$n
+			ORIG[$r]=$arg
 			r=`expr $r + 1`
+		else
+			RIM[$i]=$arg
+			i=`expr $i +1`
 		fi
-
-		n = `expr $n + 1`
 	done
+	if [ ${#ORIG[@]} -gt 0 ]
+	then
+		cal ${ORIG[*]} >> ~/.cal-extnd/.buflike
+	fi
 	if [ ${#RIM[@]} -gt 0 ]
 	then
-		cal ${RIM[*]}
+		n=1
+		for ARG in ${RIM[@]}
+		do
+			if [ $ARG -eq ${ARGS[0]} ]
+			then
+				#dosomethimg
+			elif [ $ARG -eq ${ARGS[1]} ]
+			then
+				#dosomething
+			elif [ $ARG -eq ${ARGS[2]} ]
+			then
+				#dosomething
+			elif [ $ARG -eq ${ARGS[3]} ]
+			then
+				#dosomething
+			elif [ $ARG -eq ${ARGS[4]} ]
+			then
+				#dosomething
+				XEND=1
+			else
+				#DOSOMETHINGELSE
+			fi
+			n = `expr $n + 1`
+		done
+	fi
+	if [ $XEND == 1 ]
+	then
+		if [ -s ~/.cal-enxtnd/.buflike ]
+		then
+			cat ~/.cal-extnd/.buflike
+		else
+			cal -3
+		fi
+		bash ~/.cal-extnd/mkapntmnt.sh
 	fi
 else
 	cal
 fi
-
+rm ~/.cal-extnd/.buflike
 
--- .cal-extnd/mkapntmnt.sh
+++ .cal-extnd/mkapntmnt.sh
@@ -1,4 +1,3 @@
-cal -3
 day=`date +%d`
 day=`expr $day + 1`
 month=`date +%m`
@@ -7,45 +6,46 @@ echo ''
 DAY=0
 while [ $DAY -lt 1 -o $DAY -gt 31 ]
 do
-	echo "On what day do you have the appointment?(DD/default: $day (Tomorrow)): "
-	read DAY
-	if [ $DAY -eq '' ]
+	read -p "On what day do you have the appointment?(DD/default: $day (Tomorrow)): " DAY
+	if [ -z $DAY ]
 	then
 		DAY=$day
 	elif [ $DAY -lt 1 -o $DAY -gt 31 ]
+	then
 		echo "Invalid Input! Please provide a valid day or hit return for tomorrow."
 	fi
 done
 MONTH=0
 while [ $MONTH -lt 1 -o $MONTH -gt 12 ]
 do
-	echo "On what month do you have the appointment?(MM/default: $month (This month)): "
-	read MONTH
-	if [ $MONTH -eq '' ]
+	read -p "On what month do you have the appointment?(MM/default: $month (This month)): " MONTH
+	if [ -z $MONTH ]
 	then
 		MONTH=$month
 	elif [ $MONTH -lt 1 -o $MONTH -gt 12 ]
+	then
 		echo "Invalid Input! Please provide a valid month or hit return for this month."
 	fi
 done
 YEAR=9999999
 while [ $YEAR -gt 9999 ]
 do
-	echo "In what year do you have the appointment?(YYYY/yy/default: $year (This year)): "
-	read YEAR
-	if [ $YEAR -eq '' ]
+	read -p "In what year do you have the appointment?(YYYY/yy/default: $year (This year)): " YEAR
+	if [ -z $YEAR ]
 	then
 		YEAR=$year
-	elif [ $YEAR -gt 0 -a $YEAR -lt 100 ]
+	elif [ $YEAR -gt 71 -a $YEAR -lt 100 ]
+	then
+		YEAR=`expr $YEAR + 1900`
+	elif [ $YEAR -ge 0 -a $YEAR -lt 72 ]
 	then
 		YEAR=`expr $YEAR + 2000`
 	elif [ $YEAR -gt 9999 ]
+	then
 		echo "Invalid Input! Please provide a valid year or hit return for this year."
 	fi
 done
-echo "The time of your appointment?(optional): "
-read TIME
-echo "Describe you appointment(optional): "
-read DSCRPTN
+read -p "The time of your appointment?(optional): " TIME
+read -p "Describe you appointment(optional): " DSCRPTN
 CALENTRY="$DAY $MONTH $YEAR : $TIME >> $DESCRIPTION"
 echo $CALENTRY >> calfile
