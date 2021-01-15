bf470428c12e20e5573fdd4317bebc561e9d427d
sharifmehedi24 <sharifmehedi24@yahoo.com>
2021-01-16 00:36:06 +0600

    update

--- .cal-extnd/cal-extnd.sh
+++ .cal-extnd/cal-extnd.sh
@@ -10,6 +10,9 @@ ARGS[3]="-w"
 ARGS[4]="-E"
 touch ~/.cal-extnd/.buflike
 XEND=0
+TODAY=`date +%d`
+THISMONTH=`date +%m`
+THISYEAR=`date +%Y`
 MONTH[0]="January"
 MONTH[1]="February"
 MONTH[2]="March"
@@ -47,9 +50,111 @@ then
 		n=1
 		for ARG in ${RIM[@]}
 		do
-			if [ $ARG -eq ${ARGS[0]} ]
+			if [ $ARG -eq ${ARGS[0]} -o $ARG -eq ${ARGS[1]} -o $ARG -eq ${ARGS[2]} ]
 			then
 				#dosomethimg
+				l=${RIM[$n+1]}
+				ENDDAY=`expr $l + $TODAY`
+				month=$THISMONTH
+				day=$TODAY
+				year=$THISYEAR
+				months=0
+				years=0
+				while
+				do
+					if [ $month == 1 -o $month == 3 -o $month == 5 -o $month == 7 -o $month == 8 -o $month == 10 -o $month == 12]
+					then
+						m=`expr $l % 31`
+						if [ $m == 0 ]
+						then
+							break
+						fi
+						l=`expr $l - 31`
+						months=`expr $months + 1`
+						if [ $months == 12 ]
+						then
+							years=`expr $years + 1`
+							months=0
+						fi
+						month=`expr $month + 1`
+						if [ $month == 13 ]
+						then
+							year=`expr $year + 1`
+							month=1
+						fi
+					elif [ $month == 4 -o $month == 6 -o $month == 9 -o $month == 11]
+					then
+						m=`expr $l % 30`
+						if [ $m == 0 ]
+						then
+							break
+						fi
+						l=`expr $l - 30`
+						months=`expr $months + 1`
+						if [ $months == 12 ]
+						then
+							years=`expr $years + 1`
+							months=0
+						fi
+						month=`expr $month + 1`
+						if [ $month == 13 ]
+						then
+							year=`expr $year + 1`
+							month=1
+						fi
+					else
+						leaf=`expr $year % 4`
+						if [ $leaf == 0 ]
+						then
+							m=`expr $l % 29`
+							if [ $m == 0 ]
+							then
+								break
+							fi
+							l=`expr $l - 29`
+							months=`expr $months + 1`
+							if [ $months == 12 ]
+							then
+								years=`expr $years + 1`
+								months=0
+							fi
+							month=`expr $month + 1`
+							if [ $month == 13 ]
+							then
+								year=`expr $year + 1`
+								month=1
+							fi
+						else
+							m=`expr $l % 28`
+							if [ $m == 0]
+							then
+								break
+							fi
+							l=`expr $l - 28`
+							months=`expr $months + 1`
+							if [ $months == 12 ]
+							then
+								years=`expr $years + 1`
+								months=0
+							fi
+							month=`expr $month + 1`
+							if [ $month == 13 ]
+							then
+								year=`expr $year + 1`
+								month=1
+							fi
+						fi
+					fi
+				done
+				days=$l
+				elif [ $l -ge 30 ]
+				then
+
+				while read line
+				do
+					#dosomething
+					
+				done < 'calfile'
 			elif [ $ARG -eq ${ARGS[1]} ]
 			then
 				#dosomething
